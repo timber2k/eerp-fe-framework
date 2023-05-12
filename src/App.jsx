4dd04dae088eb3b035d632591603a1cca88260b2
Tu (Timber) V. DOAN <timber@enclave.vn>
2023-05-12 13:58:53 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <>
+      <Button className="tw-bg-blue-500 tw-text-white">Click me</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
